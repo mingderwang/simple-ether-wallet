e1c2a18a5834d3898a1db9d2bb698b1ee96b39ea
Ming-der Wang <mingderwang@gmail.com>
2021-09-08 01:41:06 +0800

    ok for etherspot sign message

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,21 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { invoke } from '@forge/bridge';
+import React, { useEffect, useState } from 'react'
+import { invoke } from '@forge/bridge'
 import { ec as EC } from 'elliptic'
+import { Sdk, NetworkNames } from 'etherspot';
+
+let sdk
+
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null)
 
   useEffect(async () => {
-    const ec = new EC('secp256k1')
-    var key = await ec.genKeyPair();
-    console.info('key',key)
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+    sdk = new Sdk('0x492a0ed9e5f29e9491ad4893f16634cfc50facac1d23d6035319b708f0023acc', {
+      networkName: 'matic',
+    });
+    console.log('sdk',sdk)
+    const signature = await sdk.signMessage({
+      message: 'asdf',
+    });
+  
+    console.log('signature', signature);
 
-  return (
-    <div>
-      {data ? data : 'Loading...'}
-    </div>
-  );
+
+
+    const ec = new EC('secp256k1')
+    var key = await ec.genKeyPair()
+    console.info('key', key)
+    var msgHash = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    var signature2 = await key.sign(msgHash)
+    console.info('sign', signature2)
+    invoke('getText', { example: 'my-invoke-variable' }).then(setData)
+    
+  }, [])
+
+  return <div>{data ? data : 'Loading...'}</div>
 }
 
-export default App;
+export default App
