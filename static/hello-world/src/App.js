ec51ab5f6d9ef418788bde5135f866215bf473e0
Ming-der Wang <mingderwang@gmail.com>
2021-09-08 16:22:58 +0800

    ok for getGasInfo

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { invoke } from '@forge/bridge'
 import { ec as EC } from 'elliptic'
-import { Sdk, NetworkNames } from 'etherspot';
-
-let sdk
+import { Sdk } from 'etherspot'
+var sdk
 
 function App() {
   const [data, setData] = useState(null)
 
   useEffect(async () => {
-    sdk = new Sdk('0x492a0ed9e5f29e9491ad4893f16634cfc50facac1d23d6035319b708f0023acc', {
-      networkName: 'matic',
-    });
-    console.log('sdk',sdk)
+    sdk = new Sdk(
+      {
+        privateKey:
+          '0xe1b02eb0239ab074b79acb3d53f3114dd54fe6f989e75b680364f8899bc34ed3',
+      },
+      {
+        networkName: 'mainnet',
+      },
+    )
     const signature = await sdk.signMessage({
-      message: 'asdf',
-    });
-  
-    console.log('signature', signature);
+      message: 'deserunt aliqua mollit ullamco sit aliquip enim in laboris do',
+    })
+    const output = await sdk.getGatewayGasInfo()
 
+    console.log('gateway gas info', output)
 
-
+    console.log('signature', signature)
     const ec = new EC('secp256k1')
     var key = await ec.genKeyPair()
     console.info('key', key)
@@ -28,7 +32,6 @@ function App() {
     var signature2 = await key.sign(msgHash)
     console.info('sign', signature2)
     invoke('getText', { example: 'my-invoke-variable' }).then(setData)
-    
   }, [])
 
   return <div>{data ? data : 'Loading...'}</div>
